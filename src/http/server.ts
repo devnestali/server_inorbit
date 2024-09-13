8ff95435d8f473ffca8d54322bf18222640e3e3f
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-09-13 16:06:12 +0200

    Ending requests to BackEnd Server

--- src/http/server.ts
+++ src/http/server.ts
@@ -4,10 +4,10 @@ import {
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
-import { getWeekPendingGoals } from '../functions/get-week-pending-goal'
 import { createCompletionRoute } from './routes/create-completion'
 import { createGoalRoute } from './routes/create-goal'
 import { getPendingGoalsRoute } from './routes/get-pending-goals'
+import { getWeekSummaryRoute } from './routes/get-week-summarys'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -17,6 +17,7 @@ app.setSerializerCompiler(serializerCompiler)
 app.register(createGoalRoute)
 app.register(createCompletionRoute)
 app.register(getPendingGoalsRoute)
+app.register(getWeekSummaryRoute)
 
 app
   .listen({
